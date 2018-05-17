dcfe9700eaeaf900c32f229a6658f0f3f587563a
vlad_grigoryan <vladimir.grigoryan.1988@gmail.com>
2018-05-17 18:38:32 +0400

    change build folder to dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
         libraryTarget: 'commonjs2'
     },
@@ -11,7 +11,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|bower_components|build)/,
+                exclude: /(node_modules|bower_components|dist)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
