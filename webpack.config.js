83d4e65b141f7708903bac6932ba145f82585122
Aaron Reisman <aaron@lifeiscontent.net>
2018-08-28 20:19:13 -0700

    Add library for browser usage

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+    library: "ReactRecaptchaGoogle",
     libraryTarget: "umd"
   },
   module: {
